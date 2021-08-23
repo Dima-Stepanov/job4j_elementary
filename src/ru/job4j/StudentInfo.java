db67cd25710ca1eca95cd56e8f085c5ee7702bc7
dmitryjob4j <haoos@inbox.ru>
2021-08-23 16:57:50 +0300

    3.3.Разбор первой программы.[#159033 #127141]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+/**
+ * 3.3.Разбор первой программы.[#159033 #127141]
+ */
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Dmitry Stepanov");
+        System.out.println("23.08.2021");
+    }
+}
