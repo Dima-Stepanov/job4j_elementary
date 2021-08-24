a6c542c690501fd0adf2871d33fd9130ae0bcca1
dmitryjob4j <haoos@inbox.ru>
2021-08-24 12:17:00 +0300

    1.1.2.Тип данных.0.Что такое переменная.[#3610#127159]

---
+++ src/ru/job4j/Variables.java
@@ -0,0 +1,30 @@
+package ru.job4j;
+
+/**
+ * 1.1.2. Тип данных
+ * 0. Что такое переменная. [#3610 #127159]
+ */
+public class Variables {
+    public static void main(String[] args) {
+        byte mem = 1;
+        System.out.println(mem);
+        short size = 1;
+        System.out.println(size);
+        int length = 100500;
+        System.out.println(length);
+        long money = 900500;
+        System.out.println(money);
+        float weight = 1.05f;
+        System.out.println(weight);
+        double distance = 100500.99;
+        System.out.println(distance);
+        boolean exists = true;
+        System.out.println(exists);
+        char gender = 'Y';
+        System.out.println(gender);
+        int age = 18;
+        float leng = 72.0332f;
+        System.out.println(leng);
+        boolean baby = true;
+    }
+}
