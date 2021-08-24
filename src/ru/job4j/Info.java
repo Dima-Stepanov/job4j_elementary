0db07ea48707c036b6f5d4a4f75614c9ff1cc167
dmitryjob4j <haoos@inbox.ru>
2021-08-24 10:30:30 +0300

    3.4.Заголовок в файле java[#159036 #127135]

---
+++ src/ru/job4j/Info.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+/**
+ * 3.4. Заголовок в файле java [#159036 #127135]
+ */
+public class Info {
+    public static void main(String[] args) {
+        System.out.println("28.08.2021");
+    }
+}
