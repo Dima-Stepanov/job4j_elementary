043a5ee540edab2c4004d9f477a7da52ae6f4387
dmitryjob4j <haoos@inbox.ru>
2021-08-31 13:00:14 +0300

    1.1.4.Оператор ветвления.4.4.Вложенный оператор if else[#190#127099]

---
+++ src/ru/job4j/condition/DivideBySix.java
@@ -0,0 +1,25 @@
+package ru.job4j.condition;
+
+/**
+ * 1.1.4.Оператор ветвления
+ * 4.4.Вложенный оператор if else[#190 #127099]
+ */
+public class DivideBySix {
+    public static String checkNumber(int number) {
+        String rsl;
+        if (number % 3 == 0) {
+            if (number % 2 == 0) {
+                rsl = "Исходное число делится на 6.";
+            } else {
+                rsl = "Исходное число делится на 3, но не является четным.";
+            }
+        } else {
+            if (number % 2 == 0) {
+                rsl = "Исходное число не делится на 3, но является четным.";
+            } else {
+                rsl = "Исходное число не делится на 3 и не является четным";
+            }
+        }
+        return rsl;
+    }
+}
