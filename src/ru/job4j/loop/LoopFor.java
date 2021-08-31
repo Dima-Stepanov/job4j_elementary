54d44eab747ed9461e7e37021059b911dffcb3de
dmitryjob4j <haoos@inbox.ru>
2021-08-31 17:36:33 +0300

    1.1.5.Циклы.5.0.Цикл for.Определение.[#3572#127185]

---
+++ src/ru/job4j/loop/LoopFor.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+/**
+ * 1.1.5. Циклы
+ * 5.0.Цикл for.Определение.[#3572#127185]
+ */
+public class LoopFor {
+    public static void main(String[] args) {
+        for (int i = 5; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
