d96c3d62ab0999b866c57ddd4be381907c4c2662
dmitryjob4j <haoos@inbox.ru>
2021-09-02 16:16:14 +0300

    1.1.5.Циклы.5.9.1.Генерация конструкций в IDEA[#279134]

---
+++ src/ru/job4j/live/Loop.java
@@ -0,0 +1,20 @@
+package ru.job4j.live;
+
+/**
+ * 1.1.5.Циклы
+ * 5.9.1.Генерация конструкций в IDEA[#279134]
+ */
+public class Loop {
+    /**
+     * psvm + tab main
+     * fori + tab for(int i = 0; i < ; i++)
+     * sout + tab System.out.println
+     *
+     * @param args null
+     */
+    public static void main(String[] args) {
+        for (int i = 0; i < 11; i++) {
+            System.out.println(i);
+        }
+    }
+}
