16d5142fe8968fa291080d60e847da8935d70827
dmitryjob4j <haoos@inbox.ru>
2021-09-02 18:03:10 +0300

    1.1.6.Массивы.6.0.1.Объявление массива.[#3613#127112]

---
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,13 @@
+package ru.job4j.array;
+
+/**
+ * 1.1.6. Массивы
+ * 6.0.1.Объявление массива.[#3613#127112]
+ */
+public class ArrayDefinition {
+    public static void main(String[] args) {
+        short[] ages = new short[10];
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
+    }
+}
