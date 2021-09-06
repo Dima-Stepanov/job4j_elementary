b6de477b047cc5fd1f5b285cbc5f35ea3d9472e4
dmitryjob4j <haoos@inbox.ru>
2021-09-06 23:50:48 +0300

    1.1.6.Массивы.6.6.2.Двухмерный массив.Циклы.[#306725]

---
+++ src/main/java/ru/job4j/array/MatrixSum.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+/**
+ * 1.1.6.Массивы
+ * 6.6.2.Двухмерный массив.Циклы.[#306725]
+ */
+public class MatrixSum {
+    public static int sum(int[][] array) {
+        int rsl = 0;
+        for (int row = 0; row < array.length; row++) {
+            for (int cell = 0; cell < array[row].length; cell++) {
+                rsl += array[row][cell];
+            }
+        }
+        return rsl;
+    }
+}
