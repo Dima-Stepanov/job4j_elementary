78dc2220c50e1c3d57fecd2e1d10a9c1d2c1bcbb
dmitryjob4j <haoos@inbox.ru>
2021-09-06 11:38:45 +0300

    1.1.6.Массивы6.6.4.1.1.Обход массива с последнего элемента[#361619]

---
+++ src/ru/job4j/array/PrintEvenElemets.java
@@ -0,0 +1,18 @@
+package ru.job4j.array;
+
+/**
+ * 1.1.6.Массивы
+ * 6.4.1.1. Обход массива с последнего элемента[#361619]
+ */
+public class PrintEvenElemets {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[numbers.length - 1 - i] % 2 == 0) {
+                System.out.println("Четны элемент массива начиная с последнего: "
+                        + numbers[numbers.length - 1 - i]
+                );
+            }
+        }
+    }
+}
