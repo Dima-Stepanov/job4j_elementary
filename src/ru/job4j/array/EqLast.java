6f935e28b13634991dbee536543698b7ff1191b0
dmitryjob4j <haoos@inbox.ru>
2021-09-06 11:27:11 +0300

    1.1.6.Массивы6.4.1.Сравнить последние элементы двух массивов[#304228]

---
+++ src/ru/job4j/array/EqLast.java
@@ -0,0 +1,11 @@
+package ru.job4j.array;
+
+/**
+ * 1.1.6.Массивы
+ * 6.4.1.Сравнить последние элементы двух массивов[#304228]
+ */
+public class EqLast {
+    public static boolean check(int[] left, int[] right) {
+        return left[left.length - 1] == right[right.length - 1];
+    }
+}
---
+++ test/ru/job4j/array/EqLastTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * 1.1.6.Массивы
+ * 6.4.1.Сравнить последние элементы двух массивов[#304228]test
+ */
+public class EqLastTest {
+
+    @Test
+    public void whenEq() {
+        int[] left = {1, 2, 3};
+        int[] right = {5, 4, 3};
+        boolean result = EqLast.check(left, right);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void whenNotEq() {
+        int[] left = {1, 2, 3};
+        int[] right = {3, 3, 4};
+        boolean result = EqLast.check(left, right);
+        Assert.assertFalse(result);
+    }
+}
