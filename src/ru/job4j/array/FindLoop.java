b653eb08714c0e933cb86fc6d9ade4c10ad0181c
dmitryjob4j <haoos@inbox.ru>
2021-09-06 12:10:30 +0300

    1.1.6.Массивы.6.5.Поиск индекса в диапазоне.[#156318 #127121]

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -3,11 +3,37 @@ package ru.job4j.array;
 /**
  * 1.1.6.Массивы
  * 6.1.Классический поиск перебором.[#33489#127117]
+ * 6.5.Поиск индекса в диапазоне.[#156318 #127121]
  */
 public class FindLoop {
+    /**
+     * 6.1.Классический поиск перебором.[#33489#127117]
+     * @param data int[]
+     * @param el search element
+     * @return index data
+     */
     public static int indexOf(int[] data, int el) {
         int rsl = -1;
         for (int i = 0; i < data.length; i++) {
+            if (data[i] == el) {
+                rsl = i;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    /**
+     * 6.5.Поиск индекса в диапазоне.[#156318 #127121]
+     * @param data int[]
+     * @param el search element
+     * @param start start index search
+     * @param finish end index search
+     * @return index data
+     */
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        int rsl = -1;
+        for (int i = start; i <= finish; i++) {
             if (data[i] == el) {
                 rsl = i;
                 break;
--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -8,9 +8,12 @@ import static org.junit.Assert.*;
 /**
  * 1.1.6.Массивы
  * 6.1.Классический поиск перебором.[#33489#127117]test
+ * 6.5.Поиск индекса в диапазоне.[#156318 #127121]test
  */
 public class FindLoopTest {
-
+    /*
+    6.1.Классический поиск перебором.[#33489#127117]test
+     */
     @Test
     public void whenArrayHas5Then0() {
         int[] data = {5, 4, 3, 2};
@@ -34,6 +37,31 @@ public class FindLoopTest {
         int[] data = {5, 4, 3, 7, 8, 2, 1};
         int el = 6;
         int result = FindLoop.indexOf(data, el);
+        int expected = -1;
+        Assert.assertEquals(expected, result);
+    }
+
+    /*
+    6.5.Поиск индекса в диапазоне.[#156318 #127121]test
+     */
+    @Test
+    public void whenArrayHas6ThenStart2Finish4() {
+        int[] data = {1, 2, 3, 5, 6, 8, 10, 7};
+        int el = 6;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(data, el, start, finish);
+        int expected = 4;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenArrayHas10ThenStart0Finish5ThenNoSerch() {
+        int[] data = {1, 2, 3, 5, 6, 8, 10, 7};
+        int el = 10;
+        int start = 0;
+        int finish = 5;
+        int result = FindLoop.indexOf(data, el, start, finish);
         int expected = -1;
         Assert.assertEquals(expected, result);
     }
