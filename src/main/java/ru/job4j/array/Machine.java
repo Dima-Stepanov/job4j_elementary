c5214bcf14e9d245dee4450d870203374815ccf0
dmitryjob4j <haoos@inbox.ru>
2021-09-08 18:07:06 +0300

    1.1.6.Массивы.6.9.Жадный алгоритм.Сдача в кофе машине[#34741]

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 /**
  * 1.1.6.Массивы
  * 6.9.Жадный алгоритм.Сдача в кофе машине[#34741]
+ * fori заменил на for each
  */
 public class Machine {
     public static int[] change(int money, int price) {
@@ -12,10 +13,10 @@ public class Machine {
         int[] rsl = new int[100];
         int size = 0;
         int change = money - price;
-        for (int i = 0; i < coins.length && change > 0; i++) {
-            while (change - coins[i] >= 0) {
-                change -= coins[i];
-                rsl[size++] = coins[i];
+        for (int coin : coins) {
+            while (change - coin >= 0) {
+                change -= coin;
+                rsl[size++] = coin;
             }
         }
         return Arrays.copyOf(rsl, size);
