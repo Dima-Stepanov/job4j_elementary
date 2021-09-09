e98d677eb8f852327b31b3f772a678bc4256998a
dmitryjob4j <haoos@inbox.ru>
2021-09-09 22:26:40 +0300

    1.2.1.Классы и объекты.2.1.Рефакторинг-Расстояние между точками.[#122587#127044]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -1,24 +1,34 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
 /**
  * 1.1.2. Тип данных
  * 7. Расстояние между точками в системе координат [#188 #127151]
  * Math.sqrt(a) - корень квадратный от числа а.
  * Math.pow(a, b) - возведение числа а в степень b.
+ * 1.2.1.Классы и объекты
+ * 2.1.Рефакторинг - Расстояние между точками.[#122587#127044]
  */
 public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        double x = x2 - x1;
-        double y = y2 - y1;
-        double powX = Math.pow(x, 2);
-        double powY = Math.pow(y, 2);
-        return Math.sqrt((powX - powY));
+    private int x;
+    private int y;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2)
+                + pow(this.y - that.y, 2));
     }
 
     public static void main(String[] args) {
-        double result = Point.distance(0, 0, 2, 0);
-        System.out.println("result (0, 0) to (2, 0) " + result);
-        result = Point.distance(2, 5, 5, 3);
-        System.out.println("result (2, 5) to (5, 3) " + result);
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        System.out.println(dist);
     }
 }
--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -8,6 +8,8 @@ import static org.junit.Assert.*;
 /**
  * 1.1.3.Тестирование
  * 3.Тесты для определения расстояния между точками.[#159459#127169]Test
+ *  * 1.2.1.Классы и объекты
+ *  * 2.1.Рефакторинг - Расстояние между точками.[#122587#127044]Test
  */
 public class PointTest {
 
@@ -18,18 +20,22 @@ public class PointTest {
         int y1 = 0;
         int x2 = 2;
         int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(x1, y1);
+        Point b = new Point(x2, y2);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when13to42Then2I82Distance() {
-        double expected = 2.82;
+        double expected = 3.16;
         int x1 = 1;
         int y1 = 3;
         int x2 = 4;
         int y2 = 2;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(x1, y1);
+        Point b = new Point(x2, y2);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 
@@ -40,18 +46,22 @@ public class PointTest {
         int y1 = 0;
         int x2 = 5;
         int y2 = 0;
-        double actual = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, actual, 0.01);
+        Point a = new Point(x1, y1);
+        Point b = new Point(x2, y2);
+        double out = a.distance(b);
+        Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when13to82Then6I92Distance() {
-        double expected = 6.92;
+        double expected = 7.07;
         int x1 = 1;
         int y1 = 3;
         int x2 = 8;
         int y2 = 2;
-        double actual = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, actual, 0.01);
+        Point a = new Point(x1, y1);
+        Point b = new Point(x2, y2);
+        double out = a.distance(b);
+        Assert.assertEquals(expected, out, 0.01);
     }
 }
